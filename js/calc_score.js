847250a255c43653ab9975cd7b157c5104a0d09c
Azu Lee <yunique88@gmail.com>
2015-07-13 11:17:08 -0400

    calcscore

--- js/calc_score.js
+++ js/calc_score.js
@@ -1,6 +1,8 @@
 // Calc Score
 
-
+var line_data;
+var unq_line_data;
+var dup_real;
 function calc_score(value) {
 	// duplicate_xy_list = [];
 	dup_temp = [];
@@ -33,39 +35,47 @@ function calc_score(value) {
 		}
 	}
 
-	// remove inner surrounded cells.
-	remove_inner_cell();
+	// // reset
+	// d3.selectAll(".common_path").attr('stroke', null);
 
-	draw_path_around();
+	// // draw edges around
+	// while (dup_temp.length > 0) {
+	// 	dup_real = [];
+	// 	dup_real = get_a_set(dup_temp.shift()).slice();
+	// 	dup_real = get_unq(dup_real);
+	// 	remove_inner_cell();
+	// 	draw_path_around();
+	// 	console.log(dup_temp.length);
+	// 	prev_score = curr_score;
+	// 	curr_score = score_count;
 
-	prev_score = curr_score;
-	curr_score = score_count;
-
-	// Changes the score on the scoreboard.
-	if (curr_score > prev_score) {
-		d3.select('.score_board_text').text(curr_score)
-			.attr("font-size", 100).attr("y", (height_sc - 20) / 2)
-			.attr("fill", "green").attr("y", height_sc / 2 + 13);
-		d3.select('.score_board_text').transition().duration(800)
-			.attr("font-size", 50).attr("fill","black").attr("y", height_sc / 2 + 8);
-	} else if (curr_score < prev_score) {
-		d3.select('.score_board_text').text(curr_score)
-			.attr("font-size", 120).attr("y", (height_sc - 20) / 2)
-			.attr("fill", "red").attr("y", height_sc / 2 + 13);
-		d3.select('.score_board_text').transition().duration(800)
-			.attr("font-size", 50).attr("fill","black").attr("y", height_sc / 2 + 8);
-	}
+	// 	// Changes the score on the scoreboard.
+	// 	if (curr_score > prev_score) {
+	// 		d3.select('.score_board_text').text(curr_score)
+	// 			.attr("font-size", 100).attr("y", (height_sc - 20) / 2)
+	// 			.attr("fill", "green").attr("y", height_sc / 2 + 13);
+	// 		d3.select('.score_board_text').transition().duration(800)
+	// 			.attr("font-size", 50).attr("fill","black").attr("y", height_sc / 2 + 8);
+	// 	} else if (curr_score < prev_score) {
+	// 		d3.select('.score_board_text').text(curr_score)
+	// 			.attr("font-size", 120).attr("y", (height_sc - 20) / 2)
+	// 			.attr("fill", "red").attr("y", height_sc / 2 + 13);
+	// 		d3.select('.score_board_text').transition().duration(800)
+	// 			.attr("font-size", 50).attr("fill","black").attr("y", height_sc / 2 + 8);
+	// 	}
+	// }
 }
 
 
-var line_data;
-var unq_line_data;
 // gets list of cell coordinates, find out outer boarder, draws lines around it.
 function draw_path_around() {
+
 	line_data = [];
 
 	get_edges_of_all();
+	
 	order_edges();
+
 
 	var lineFunction = d3.svg.line()
 			.x(function(d) { return d.x; })
@@ -80,6 +90,7 @@ function draw_path_around() {
 
 	// Draws the line
 	var lineGraph = svg.append("path").transition().duration(1000)
+			.attr('class', 'common_path')
 			.attr("d", lineFunction(line_data))
 			.attr("stroke", "white")
 			.attr("stroke-width", 2)
@@ -98,8 +109,8 @@ function unq_line_data_contains(value) {
 
 // recursive funtion to add all 
 function get_edges_of_all() {
-	for(var i = 0; i < 2; i++) {
-		var temp = get_four_corners(dup_temp[i]).slice();
+	for(var i = 0; i < dup_real.length; i++) {
+		var temp = get_four_corners(dup_real[i]).slice();
 		for (j = 0; j < 4; j++)
 			line_data.push(temp[j]);
 	}
@@ -122,24 +133,51 @@ function get_edges_of_all() {
 			}
 		}
 	}
-	console.log(count_unq_line_data);
-	var remove = [];
-	for (var i = 0; i < unq_line_data.length; i++) {
+
+
+	var remove2 = [];
+	var remove3 = [];
+	var remove4 = [];
+	for (var i = 0; i < count_unq_line_data.length; i++) {
 		if (count_unq_line_data[i] == 2) {
-			remove.push(unq_line_data[i]);
+			remove2.push(unq_line_data[i]);
 		} else if (count_unq_line_data[i] == 3) {
-			remove.push(unq_line_data[i]);
-			remove.push(unq_line_data[i]);
+			remove3.push(unq_line_data[i]);
 		} else if (count_unq_line_data[i] == 4) {
-			remove.push(unq_line_data[i]);
-			remove.push(unq_line_data[i]);
-			remove.push(unq_line_data[i]);
-			remove.push(unq_line_data[i]);
+			remove4.push(unq_line_data[i]);
 		}
 	}
 
-	for (var i = 0; i < remove.length; i++) {
-		line_data.splice(line_data.indexOf(remove[i]),1);
+
+	// removes duplicates
+	for (var i = 0; i < remove2.length; i++) {
+		var once = 0;
+		for (var j = line_data.length - 1; j >= 0; j--) {
+			if (is_point_equal(remove2[i], line_data[j]) && once < 1 ) {
+				line_data.splice(j, 1);
+				once++;
+			}
+		}
+	}
+
+	// removes 3 !!!!
+	for (var i = 0; i < remove3.length; i++) {
+		var twice = 0;
+		for (var j = line_data.length - 1; j >= 0; j--) {
+			if (is_point_equal(remove3[i], line_data[j]) && twice < 2 ) {
+				line_data.splice(j, 1);
+				twice++;
+			}
+		}
+	}
+
+	// removes 4
+	for (var i = 0; i < remove4.length; i++) {
+		for (var j = line_data.length - 1; j >= 0; j--) {
+			if (is_point_equal(remove4[i], line_data[j])) {
+				line_data.splice(j, 1);
+			}
+		}
 	}
 }
 
@@ -147,59 +185,66 @@ function order_edges() {
 	var new_line_data = [];
 	var next = line_data.shift();
 	new_line_data.push(next);
-	console.log(next);
-	console.log(whats_next(next));
-	// var new_line_data = [];
-	// new_line_data.push(next);
-	// while (line_data.length > 0) {
-		// next = whats_next(next);
-		// line_data.splice(line_data.indexOf(next), 1);
-		// new_line_data.push(next);
-	// }
-	// line_data = new_line_data.slice();
-	// console.log(new_line_data);
-	console.log(line_data);
-	console.log(new_line_data);
+
+	while (line_data.length > 0) {
+		var temp_ind = whats_next(next);
+		next = line_data[temp_ind];
+		new_line_data.push(next);
+		line_data.splice(temp_ind,1);
+	}
+	line_data = new_line_data.slice();
 }
 
 function remove_inner_cell() {
 	var delete_index = [];
-	for (var i = 0; i < dup_temp.length; i++) {
-		var curr = dup_temp[i];
+	for (var i = 0; i < dup_real.length; i++) {
+		var curr = dup_real[i];
 		if (has_up(curr) != undefined && has_down(curr) != undefined
 			&& has_left(curr) != undefined && has_right(curr) != undefined)
 			delete_index.push(i);
 	}
 	var temp = delete_index.length
 	for(var i = 0; i < temp; i++) {
-		dup_temp.splice(delete_index[i], 1);
+		dup_real.splice(delete_index[i], 1);
 	}
 }
 
 function whats_next(value) {
-	console.log(value);
 	var north = { "x": value.x, "y": value.y - scale_xy.x(1)};
 	var south = { "x": value.x, "y": value.y + scale_xy.x(1)};
 	var east  = { "x": value.x + scale_xy.y(1), "y": value.y};
-	var west  = { "x": value.x - scale_xy.y(1), "y": scale_xy.x(value.y)};
-	console.log(line_data.indexOf(north));
-	console.log(line_data.indexOf(south));
-	console.log(line_data.indexOf(east));
-	console.log(line_data.indexOf(west));
-	// console.log(line_data);
-	if (line_data.indexOf(north) != undefined) {
-		return north;
-	} else if (line_data.indexOf(south) != undefined) {
-		return south;
-	} else if (line_data.indexOf(east) != undefined) {
-		return east;
-	} else if (line_data.indexOf(west) != undefined) {
-		return west;
+	var west  = { "x": value.x - scale_xy.y(1), "y": value.y};
+	
+	var index = -1;
+
+	for (var i = 0; i < line_data.length; i++) {
+		if (is_point_equal(line_data[i], north) ) {
+				index = i;
+			}
 	}
+	for (var i = 0; i < line_data.length; i++) {
+		if (is_point_equal(line_data[i], east) ) {
+				index = i;
+			}
+	}
+	for (var i = 0; i < line_data.length; i++) {
+		if (is_point_equal(line_data[i], south) ) {
+				index = i;
+			}
+	}
+	for (var i = 0; i < line_data.length; i++) {
+		if (is_point_equal(line_data[i], west) ) {
+				index = i;
+			}
+	}
+	return index;
 }
 
 function is_point_equal(value1, value2) {
-	if (value1.x == value2.x && value1.y == value2.y) {
+	if (value1.x == undefined || value1.y == undefined 
+		|| value2.x == undefined || value2.y == undefined) {
+		return false;
+	} else if (value1.x.toFixed(2) == value2.x.toFixed(2) && value1.y.toFixed(2) == value2.y.toFixed(2)) {
 		return true;
 	} else {
 		return false;
@@ -239,20 +284,70 @@ function has_left(value) {
 	return ind[0];
 }
 
-function merge(value) {
-	get_four_corners(value);
-	for (i = 0; i < line_data.length; i++) {
-		if (line_data[i].x == one.x && line_data[i].y == one.y)
-		div_index = i;
-	}
-	var first_half = line_data.slice(0,div_index);
-	var second_half = line_data.slice(div_index + 2, line_data.length);
-	var new_line_data = first_half.slice();
+function has_neighbor(value) {
+	if (has_left(value) != undefined) return has_left(value);
+	else if (has_right(value) != undefined) return has_right(value);
+	else if (has_up(value) != undefined) return has_up(value);
+	else if (has_down(value) != undefined) return has_down(value);
+	else return -1;
 }
 
 
+function get_a_set(value) {
+	var returner = [];
+	returner.push(value);
 
+	if (has_left(value) != undefined) {
+		returner.push(dup_temp[has_left(value)]);
+		dup_temp.splice(has_left(value), 1);
+		var temp = get_a_set({x:value.x - 1, y:value.y});
+		for (var i = 0; i < temp.length; i++) {
+			returner.push(temp[i]);
+		}
+	}
+	if (has_up(value) != undefined) {
+		returner.push(dup_temp[has_up(value)]);
+		dup_temp.splice(has_up(value), 1);
+		var temp = get_a_set({x:value.x, y:value.y - 1});
+		for (var i = 0; i < temp.length; i++) {
+			returner.push(temp[i]);
+		}
+	}
+	if (has_right(value) != undefined) {
+		returner.push(dup_temp[has_right(value)]);
+		dup_temp.splice(has_right(value), 1);
+		var temp = get_a_set({x:value.x + 1, y:value.y});	
+		for (var i = 0; i < temp.length; i++) {
+			returner.push(temp[i]);
+		}
+	}
+	if (has_down(value) != undefined) {
+		returner.push(dup_temp[has_down(value)]);
+		dup_temp.splice(has_down(value), 1);
+		var temp = get_a_set({x:value.x, y:value.y + 1});
+		for (var i = 0; i < temp.length; i++) {
+			returner.push(temp[i]);
+		}
+	} 
+	return returner;
+}
 
+function get_unq(value) {
+	var temp = value.slice();
+	var returner =[];
+	for (var i = 0; i < temp.length; i++) {
+		var found = false;
+		for (var j = 0; j < returner.length; j++) {
+			if (temp[i].x == returner[j].x && temp[i].y == returner[j].y) {
 
+				found = true;
+			}
+		}
+		if (!found) {
+			returner.push(temp[i]);			
+		}
+	}
+	return returner.slice();
+}
 
 
