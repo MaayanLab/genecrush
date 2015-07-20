69234e95fbdd201d5e6c9a2999949ce8e52cf96b
Azu Lee <yunique88@gmail.com>
2015-07-20 15:12:54 -0400

    last working file before branch

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
 
-transc_names = ['Disease_Signatures_from_GEO_down', 'ENCODE_Histone_Modifications_2015', 'ESCAPE', 'Mouse_Gene_Atlas', 'Virus_Perturbations_from_GEO_down']
-// Get random sample from json folder.
-random = Math.floor(Math.random() * transc_names.length);
-var path = "json/from_enrichr/";
-path += transc_names[random];
-path += '.json'
-var width = 500;
-
-d3.json(path, function(sample) {
-  var max = 0;
-  // Alphabetically order the genes.
-  var term = Object.keys(sample);
-  for (var i = 0; i < term.length; i++) {
-    if (max < sample[term[i]].length) max = sample[term[i]].length;
-  }
-  width = max * 50
-});
-
 // CONSTANTS.
 var margin = { top: 50, right: 10, bottom: 10, left: 250 },
-    // width = 800,
+    width = 800,
     height = 800,
     trans_duration = 2000,    // ms
     default_duration = 300,   // ms
@@ -31,11 +13,6 @@ var margin = { top: 50, right: 10, bottom: 10, left: 250 },
     curr_mat = [],
     curr_score = 0,
     prev_score = 0;
-
-// function zoomed() {
-//   svg.select(".x.axis").call(xAxis);
-//   svg.select(".y.axis").call(yAxis);
-// }
 
 // Main SVG.
 var svg = d3.select("#GAME").append("svg")
@@ -71,7 +48,13 @@ var curr_duration = default_duration,
     toggle_hl = 0,
     click_state = 0;
 
-
+transc_names = ['Disease_Signatures_from_GEO_down', 'ENCODE_Histone_Modifications_2015', 'ESCAPE', 'Mouse_Gene_Atlas', 'Virus_Perturbations_from_GEO_down']
+// Get random sample from json folder.
+random = Math.floor(Math.random() * transc_names.length);
+var path = "json/from_enrichr/";
+path += transc_names[random];
+path += '.json'
+var width = 0;
 
 svg.append("text")
   .attr("class", "library1")
@@ -94,14 +77,13 @@ svg.append("text")
   .on('mouseover', function () { d3.select('.library2').style('fill', 'blue'); })
   .on('mouseout', function() { d3.select('.library2').style('fill', 'skyblue'); })
   .on("click", function() { window.open("http://amp.pharm.mssm.edu/Enrichr/#stats"); });
-  // .attr("xlink:href", "http://en.wikipedia.org/wiki/");
 
 
 
 
 // d3.json("json/perfect_example.json", function(sample) {
-// d3.json("json/Phosphatase_Substrates_SAMPLE.json", function(sample) {
-d3.json(path, function(sample) {
+d3.json("json/Phosphatase_Substrates_SAMPLE.json", function(sample) {
+// d3.json(path, function(sample) {
 
   // Alphabetically order the genes.
   var term = Object.keys(sample);
